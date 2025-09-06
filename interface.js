c066279e651e8b983039b26559d9079099bdec5d
ragavan-cyper <ragavanragavan906@gmail.com>
2025-09-06 18:38:33 +0530

    add

--- interface.js
+++ interface.js
@@ -1,3 +1,5 @@
+
+
 let getweatherdata=async (city) => {
     let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c029366f72caac3a514862dc2901fd9b&units=metric`;
     let weatherkey= await fetch(weatherApi)
@@ -6,12 +8,17 @@ let getweatherdata=async (city) => {
 
     
 }
+ 
 
 let weatherbtn=document.getElementById("weatherbtn")
 weatherbtn.addEventListener("click",async () => {
 
-
-    let cityInput=document.getElementById("cityInput").value
+     let cityInput=document.getElementById("cityInput").value
+  if (cityInput.length===0){
+        document.getElementById("result").innerText = `Please enter a city name`
+                return; 
+  }
+     
     getweatherdata(cityInput).then((response)=>{
           console.log(response)
         document.getElementById("cityName").innerHTML = `
@@ -20,9 +27,15 @@ weatherbtn.addEventListener("click",async () => {
             <p>☁ Weather: ${response.weather[0].description}</p>
             <p>💧 Humidity: ${response.main.humidity}%</p>
         `;
+if (response.cod==404) {
+    document.getElementById("result").innerText = "Please enter a Correct City Name";
+        return; 
+    
+}
+
     }).catch((reject)=>{
-         document.getElementById("cityName").innerHTML = `
-            <h2>${reject}</h2>`
+        document.getElementById("result").innerText = `
+            <p>${reject}</p>`
     })
     
 })
